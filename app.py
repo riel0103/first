c7a71b6dd6d78e7340d76f09f1d2c4bd892380ac
riel0103 <109637746+riel0103@users.noreply.github.com>
2022-07-21 12:04:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import sqlite3
+import pandas as pd
+import os.path
 
 con = sqlite3.connect('db.db')
 cur = con.cursor()
@@ -19,13 +21,40 @@ if menu == '로그인':
     login_pw = st.text_input('비밀번호', placeholder='비밀번호를 입력하세요.', type='password')
 
     login_btn = st.button('로그인')
+    st.sidebar.subheader('로그인')
     if login_btn:
         user_info = login_user(login_id, login_pw)
-        st.write(user_info[4], '님 환영합니다.')
-    st.sidebar.subheader('로그인')
+        file_name = './img/'+user_info[0]+'.png'
+
+        if os.path.exists(file_name):
+            st.sidebar.image(file_name)
+            st.sidebar.write(user_info[4], '님 환영합니다.')
+        else:
+            st.write(user_info[4], '님 환영합니다.')
 if menu == '회원가입':
     st.subheader('회원가입')
-    st.sidebar.write('회원가입')
+    if menu == '회원가입':
+
+        st.info('다음 양식을 모두 입력 후 제출합니다.')
+        uid = st.text_input('아이디', max_chars=10)
+        uname = st.text_input('성명', max_chars=10)
+        upw = st.text_input('비밀번호', type='password')
+        upw_chk = st.text_input('비밀번호 확인', type='password')
+        uage = st.text_input('생년월일')
+        ugender = st.radio('성별', options=['남', '여'], horizontal=True)
+
+        ubtn = st.button('회원가입')
+        if ubtn:
+            if upw != upw_chk:
+                st.error('비밀번호가 일치하지 않습니다.')
+                st.stop()
+
+            cur.execute(f"INSERT INTO users(id, pwd, gender, age, name) "
+                        f"VALUES('{uid}', '{upw}', '{ugender}', {uage}, '{uname}')")
+            st.success('회원가입에 성공했습니다.')
+            con.commit()
 if menu == '회원목록':
     st.subheader('회원목록')
+    df = pd.read_sql('SELECT name,age,gender FROM users', con)
+    st.dataframe(df)
     st.sidebar.write('회원목록')
