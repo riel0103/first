ae5c2d5b5544b07b3fd0c363a8a724ee2b93f07f
riel0103 <109637746+riel0103@users.noreply.github.com>
2022-07-22 10:53:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import streamlit as st
-import sqlite3
-import pandas as pd
 import os.path
+import sqlite3
+
+import pandas as pd
+from PIL import Image
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
 con = sqlite3.connect('db.db')
 cur = con.cursor()
@@ -12,7 +15,7 @@ def login_user(id, pw):
                 f" WHERE id='{id}' and pwd= '{pw}'")
     return cur.fetchone()
 
-menu = st.sidebar.selectbox('MENU', options=['로그인', '회원가입', '회원목록'])
+menu = st.sidebar.selectbox('MENU', options=['로그인', '회원가입', '회원목록', '그림판'])
 
 if menu == '로그인':
     st.subheader('로그인')
@@ -58,3 +61,37 @@ if menu == '회원목록':
     df = pd.read_sql('SELECT name,age,gender FROM users', con)
     st.dataframe(df)
     st.sidebar.write('회원목록')
+
+drawing_mode = st.sidebar.selectbox(
+    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+)
+
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+if drawing_mode == 'point':
+    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    background_image=Image.open(bg_image) if bg_image else None,
+    update_streamlit=realtime_update,
+    height=150,
+    drawing_mode=drawing_mode,
+    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    key="canvas",
+)
+
+if canvas_result.image_data is not None:
+    st.image(canvas_result.image_data)
+if canvas_result.json_data is not None:
+    objects = pd.json_normalize(canvas_result.json_data["objects"])  # need to convert obj to str because PyArrow
+    for col in objects.select_dtypes(include=['object']).columns:
+        objects[col] = objects[col].astype("str")
+    st.dataframe(objects)
